4e748775c81425fa5751a570af32cc11d45e9e80
Dankhummus <danstarkorobok198@gmail.com>
2021-07-13 10:42:46 -0400

    imported pytest experimented with xfail as well as skip markers

--- test_chapter4.py
+++ test_chapter4.py
@@ -1,17 +1,15 @@
 import pytest
-@pytest.mark.xfail()
+from collections import namedtuple
+from tasks_proj.src import tasks
+
+
 def test_practise():
     assert (1, 2, 3) == (1, 2, 3)
 
-
+@pytest.mark.xfail()
 def test_practise1():
     assert (1, 2, 3) == (3, 2, 1)
 
 
 def test_practise2():
     assert (1, 2, 3, 4) == (4, 3, 2, 1)
-
-
-
-
-
